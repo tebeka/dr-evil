242051a0505c888dd177038dd308802ad9c10eb6
Miki Tebeka <miki.tebeka@gmail.com>
2010-11-19 13:45:15 -0800

    wrapped-expr

--- src/dr/evil.clj
+++ src/dr/evil.clj
@@ -1,17 +1,22 @@
 (ns dr.evil
   (:use compojure.core)
   (:use ring.util.response)
+  (:use [clojure.stacktrace :only (print-stack-trace)])
   (:use [clojure.contrib.json :only (json-str)]))
 
 (defn input-page []
   (slurp "src/index.html"))
 
+; FIXME: Give use ability to change namespace
+(defn wrapped-expr [expr]
+  (str "(binding [*print-length* 100] (do (in-ns 'dr.evil) " expr "))"))
+
 (defn eval-expr [expr]
   (try
-    (let [result (load-string (str "(in-ns 'dr.evil) " expr))]
+    (let [result (load-string (wrapped-expr expr))]
       { :result (with-out-str (println result)) :error false })
     (catch Exception e
-      (let [result (with-out-str (.printStackTrace e))]
+      (let [result (with-out-str (print-stack-trace e))]
         { :result result :error true }))))
 
 (defn evil [params]
