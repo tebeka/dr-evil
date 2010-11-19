480de6073a609d8b451ef708138161dde4c57ad1
Miki Tebeka <miki.tebeka@gmail.com>
2010-11-19 12:56:17 -0800

    lein-run

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring "0.3.2"]
-                 [compojure "0.4.1"]])
+                 [compojure "0.4.1"]]
+  :dev-dependencies [[lein-run "1.0.0-SNAPSHOT"]])
