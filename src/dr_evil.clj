ff338943e28dd7c58392e4bb305bcee2e2e22c65
Miki Tebeka <miki.tebeka@gmail.com>
2010-11-19 12:56:39 -0800

    Initial eval

--- src/dr_evil.clj
+++ src/dr_evil.clj
@@ -1,16 +1,21 @@
-(ns dr.evil)
+(ns dr.evil
+  (:use [clojure.contrib.json :only (json-str)]))
 
 (defn input-page []
   (slurp "src/index.html"))
 
 (defn eval-expr [expr]
-  (
+  (try
+    (let [result (load-string (str "(in-ns 'dr.evil) " expr))]
+      { :result (with-out-str (println result)) :error false })
+    (catch Exception e
+      (let [result (with-out-str (.printStackTrace e))]
+        { :result result :error true }))))
 
 (defn evil [params]
-  (let [expr (params "expr")]
-    (
+  (json-str (eval-expr (params "expr"))))
 
-(defroutes handler
+(defroutes app
   (GET "/" [] (input-page))
   (POST "/" {params :params} (evil params))
   (ANY "/*" [path] (redirect "/"))
