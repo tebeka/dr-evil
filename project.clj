08c04231a55f4ae7dd9e0b9570490c2ee476c095
Miki Tebeka <miki.tebeka@gmail.com>
2010-11-21 14:22:07 -0800

    Move ring to dev

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "The evil web debugger"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [ring "0.3.2"]
                  [compojure "0.4.1"]]
-  :dev-dependencies [[lein-run "1.0.0-SNAPSHOT"]])
+  :dev-dependencies [[lein-run "1.0.0-SNAPSHOT"]
+                     [ring "0.3.2"]])
