7ab21bacee1c07c8647e0aa6e8ba5ad4316112ed
Miki Tebeka <miki.tebeka@gmail.com>
2010-11-21 20:00:40 -0800

    Some tests

--- test/dr_evil_test.clj
+++ test/dr_evil_test.clj
@@ -1,6 +1,15 @@
 (ns dr-evil-test
-  (:use [dr-evil] :reload-all)
+  (:use [dr.evil] :reload-all)
   (:use [clojure.test]))
 
-(deftest dr-evil-test
-  (is 1 1))
+(deftest eval-expr-test
+  (is (= (eval-expr "(+ 1 1)") {:result "2\n" :error false}))
+  (is (:error (eval-expr "(/ 1 0)"))))
+
+(deftest html-test
+  (is (= (subs (html "/blah") 0 6) "<html>"))
+  (is (re-find (re-pattern "/blah") (html "/blah"))))
+
+(deftest evil-test
+  (is (= (subs (evil "/blah" nil) 0 6) "<html>"))
+  (is (= (evil "/blah" "(+ 2 3)") "{\"result\":\"5\\n\",\"error\":false}")))
