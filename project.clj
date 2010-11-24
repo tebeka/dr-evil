708be30aff02eaafebcf8debfe3efebe11faa499
Miki Tebeka <miki.tebeka@gmail.com>
2010-11-23 17:16:11 -0800

    Author and Url

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject dr-evil "1.0.0-SNAPSHOT"
   :description "The evil web debugger"
+  :author "Miki Tebeka <miki.tebeka@gmail.com>"
+  :url "https://bitbucket.org/tebeka/dr-evil"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.4.1"]]
