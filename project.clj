1d83eed10ca2acf03bb2401975c751fbb8bbbefb
Miki Tebeka <miki.tebeka@gmail.com>
2010-12-06 18:06:18 -0800

    ring 0.3.5

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.5.3"]]
-  :dev-dependencies [[ring "0.3.2"]])
+  :dev-dependencies [[ring "0.3.5"]])
